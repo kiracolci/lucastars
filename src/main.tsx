0380e4929555d79f12764feaeb4f471bb7b5898c
kiracolci <116026862+kiracolci@users.noreply.github.com>
2025-05-21 10:08:59 +0200

    Update with latest changes

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,8 @@ import { ConvexAuthProvider } from "@convex-dev/auth/react";
 import { ConvexReactClient } from "convex/react";
 import "./index.css";
 import App from "./App";
+<link rel="icon" type="image/png" href="/favicon.png" />
+
 
 const convex = new ConvexReactClient(import.meta.env.VITE_CONVEX_URL as string);
 
